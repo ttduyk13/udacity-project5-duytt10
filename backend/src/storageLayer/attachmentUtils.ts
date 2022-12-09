bdb5002528293648b48115095480f0797ae6022d
ttduyk13 <ttduyk13@gmail.com>
2022-12-09 07:18:26 +0700

    backend: set lambda timeout and add action getobjet for S3, upgrade deleteAttachment with check exist object

--- backend/src/storageLayer/attachmentUtils.ts
+++ backend/src/storageLayer/attachmentUtils.ts
@@ -1,50 +1,62 @@
 import * as AWS from 'aws-sdk'
-import {S3} from 'aws-sdk'
+import { S3 } from 'aws-sdk'
 import { createLogger } from '../utils/logger'
+import * as createError from 'http-errors'
 
 const logger = createLogger('S3Storage')
+
 export class S3Storage {
-    constructor(
-        private readonly s3: S3 = new AWS.S3({signatureVersion: 'v4'}),
-        private readonly bucketName = process.env.ATTACHMENT_S3_BUCKET,
-        private readonly urlExpiration = process.env.SIGNED_URL_EXPIRATION
-    ) {
+  private defaultUrlExpiration: number = 300
+
+  constructor(
+    private readonly s3: S3 = new AWS.S3({ signatureVersion: 'v4' }),
+    private readonly bucketName = process.env.ATTACHMENT_S3_BUCKET,
+    private readonly urlExpiration = process.env.SIGNED_URL_EXPIRATION
+  ) {
+  }
+
+  getBucketName = (): string => {
+    return this.bucketName
+  }
+
+
+  getUploadUrl = (todoId: string, userId: string): string => {
+    const params = {
+      Bucket: this.bucketName,
+      Key: `${userId}-${todoId}.png`,
+      Expires: Number.isInteger(this.urlExpiration)
+        ? parseInt(this.urlExpiration)
+        : this.defaultUrlExpiration
     }
+    return this.s3.getSignedUrl('putObject', params)
+  }
 
-    private defaultUrlExpiration: number = 300
+  deleteAttachment = async (todoId: string, userId: string): Promise<void> => {
 
-    getBucketName = (): string => {
-        return this.bucketName
+    const params = {
+      Bucket: this.bucketName,
+      Key: `${userId}-${todoId}.png`
     }
-
-
-    getUploadUrl = (todoId: string, userId: string): string => {
-        const params = {
-            Bucket: this.bucketName,
-            Key: `${userId}-${todoId}.png`,
-            Expires: Number.isInteger(this.urlExpiration)
-                ? parseInt(this.urlExpiration)
-                : this.defaultUrlExpiration
-        }
-        return this.s3.getSignedUrl('putObject', params)
+    try {
+      await this.s3.getObject(params).promise()
+      console.log('File existed')
+      try {
+        await this.s3.deleteObject(params).promise()
+        logger.info(`File ${params.Key} deleted`)
+      } catch (e) {
+        logger.error('Exception: ', e)
+        createError(400, JSON.stringify(e))
+      }
+    } catch (e) {
+      if (e.statusCode === 404) {
+        logger.info(`File ${params.Key} not found`)
+        createError(400, {
+          message: `File ${params.Key} not found`
+        })
+      } else {
+        logger.error('Exception: ', e)
+        createError(400, JSON.stringify(e))
+      }
     }
-
-    deleteAttachment = async (todoId: string, userId: string): Promise<void> => {
-
-        const params = {
-            Bucket: this.bucketName,
-            Key: `${userId}-${todoId}.png`,
-        }
-        try {
-            await this.s3.headObject(params).promise();
-            await this.s3.deleteObject(params).promise();
-            logger.info(`File ${params.Key} deleted`)
-        } catch (e) {
-            if (e.statusCode === 404) {
-                logger.info(`File ${params.Key} not found`);
-            } else {
-                logger.error("Exception: ", e)
-            }
-        }
-    }
+  }
 }
